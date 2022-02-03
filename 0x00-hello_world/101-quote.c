0792530f786dea28d8cb85650efb2ad516d3d289
oxyvans <benja.1999.g@hotmail.com>
2022-02-03 10:01:23 -0800

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - puts funcion
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59); 
+return (1);
+}
