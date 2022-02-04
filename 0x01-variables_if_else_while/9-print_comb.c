f1a05ca88d39bbf6c0efb232debc5eba05199ab4
oxyvans <benja.1999.g@hotmail.com>
2022-02-04 08:50:41 -0800

    coma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - puts funcion
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+if(c != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar(10);
+
+return (0);
+}
