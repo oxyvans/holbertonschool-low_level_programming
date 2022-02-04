892d1be9d1145affc2bd181b0363289197ba59fd
oxyvans <benja.1999.g@hotmail.com>
2022-02-04 07:06:14 -0800

    alphabet-e-q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - puts funcion
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char ch;
+  ch = 'a';
+  while (ch <= 'z'){
+    if ((ch == 'e') || (ch == 'q'))
+      ch++;
+    putchar(ch);
+    ch++;
+  }
+  putchar('\n');
+return (0);
+}
