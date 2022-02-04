9dfe7edc5588a2c22bb7b78d390960e976f664da
oxyvans <benja.1999.g@hotmail.com>
2022-02-04 11:13:49 -0800

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,24 @@
  */
 int main(void)
 {
- 	int i, j;
-	for (i = 0; i <= 99; i++)
- 	{
-		for (j = i + 1; j <= 99; j++)
-		{
-			putchar(((i / 10) % 10) + '0');	
-			putchar(((i / 1) % 10) + '0');
-			putchar(' ');
-			putchar(((j / 10) % 10) + '0');	
-			putchar(((j / 1) % 10) + '0');
-			if(i == 98 && j == 99){
-			  break;
-			}
-			putchar(',');
-		}
-	}
-	putchar(10);
-	return (0);
+int i, j;
+for (i = 0; i <= 99; i++)
+{
+for (j = i + 1; j <= 99; j++)
+{
+putchar(((i / 10) % 10) + '0');
+putchar(((i / 1) % 10) + '0');
+putchar(' ');
+putchar(((j / 10) % 10) + '0');
+putchar(((j / 1) % 10) + '0');
+if (i == 98 && j == 99)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar(10);
+return (0);
 }
