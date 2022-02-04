35ff60438b96d5a5196863f265b779048d36d27a
oxyvans <benja.1999.g@hotmail.com>
2022-02-04 07:57:00 -0800

    alphabrt-e-q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,16 @@
  */
 int main(void)
 {
-  char ch;
-  ch = 'a';
-  while (ch <= 'z'){
-    if ((ch == 'e') || (ch == 'q'))
-      ch++;
-    putchar(ch);
-    ch++;
-  }
-  putchar('\n');
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+if ((ch == 'e') || (ch == 'q'))
+ch++;
+putchar(ch);
+ch++;
+}
+putchar('\n');
+
 return (0);
 }
