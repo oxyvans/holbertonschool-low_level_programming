52720fefb13e04eaaab6b9e29579cf77869f2b38
oxyvans <benja.1999.g@hotmail.com>
2022-02-07 06:21:38 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _isalpha - puts funcion
+ * @n : n int
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+{
+_putchar('+');
+return (1);
+}
+}
+}    
+
