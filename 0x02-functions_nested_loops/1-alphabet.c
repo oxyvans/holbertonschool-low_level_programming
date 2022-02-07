efcc99aa05659d4f5f98d226e24861171bf4c2f7
oxyvans <benja.1999.g@hotmail.com>
2022-02-07 04:55:51 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - puts funcion
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
