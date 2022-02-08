ad3bd89eddef4726e9e668f7249658c759bf6d46
oxyvans <benja.1999.g@hotmail.com>
2022-02-07 16:14:52 -0800

    -40line

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,48 +5,33 @@
  * @n : int
  * Return: Always 0 (Success)
  */
-
 void print_times_table(int n)
 {
-if (n > 0 && n <= 15)
-{
-int a, b, c;
-for (a = 0; a <= n; a++)
-{
-for (b = 0; b <= n; b++)
-{
-c = a * b;
-if (c < 10)
-{
-if (b != 0)
-{
-_putchar(' ');
-_putchar(' ');
-}
-_putchar((c % 10) + '0');
-}
-else
-{
-if (c >= 100)
-{
-_putchar(((c / 10) / 10) + '0');
-_putchar(((c / 10) % 10) + '0');
-_putchar((c % 10) + '0');
-}
-else
-{
-_putchar(' ');
-_putchar((c / 10) + '0');
-_putchar((c % 10) + '0');
-}
-}
-if (b != n)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-_putchar('\n');
-}
-}
+  if (n == 0)
+    printf("%d\n", n);
+  if (n > 0 && n <= 15)
+    {
+      int a, b, c;
+      for (a = 0; a <= n; a++)
+	{
+	  for (b = 0; b <= n; b++)
+	    {
+	      c = a * b;
+	      if (c < 10)
+		{
+		  if (b != 0)
+		    printf("%s", "  ");
+		  printf("%d", c);
+		}
+	      else
+		  if (c >= 100)
+		    printf("%d", c);
+		  else
+		    printf("%s %d", "", c);
+	      if (b != n)
+		printf("%s", ", ");
+	    }
+	  printf("\n");
+	}
+    }
 }
