844ea4ae58c44a0dda4987bf34492848a75e0c32
oxyvans <benja.1999.g@hotmail.com>
2022-02-08 08:28:06 -0800

    fix

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
+void positive_or_negative(int);
+int largest_number(int, int, int);
+int convert_day(int, int);
 int main(void);
 #endif /* MAIN_H */
