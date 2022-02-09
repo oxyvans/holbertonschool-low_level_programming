49baf437f4ddcd4ff7ac89b483cfaea2199f1019
oxyvans <benja.1999.g@hotmail.com>
2022-02-09 11:04:10 -0800

    101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,19 @@
 #include "main.h"
-#include <math.h>
-#include <stdio.h>
 
 /**
  * print_number - Entry point
- * 
+ * @n : int
  * Return: 0
  */
 
-
-static int potencia(int a, int b){
-  int c = 1;
-  while (c <= b){
-    a = a * a;
-    c++;
-  }
-  return (a);
-}
-
-
 void print_number(int n)
 {
-  int number = n;
-  int n_digit = 0;
-  int k;
-  int cifra;
-  int p;
-  while (number > 0){
-    n_digit++;
-    number = (number / 10);
-  }
-   for (k = (n_digit-1); k >= 0; k--){
-    p = potencia(10,k);
-    cifra = ((n / p) % 10);
-    _putchar(cifra + '0'); 
-  }
-   _putchar((n % 10) + '0');
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n / 10)
+print_number(n / 10);
+_putchar((n % 10) + '0');
 }
