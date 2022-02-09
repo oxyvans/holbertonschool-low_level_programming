90b5f4e75a34d99c1a742d7362e0255fb2bb300a
oxyvans <benja.1999.g@hotmail.com>
2022-02-09 04:48:02 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+int i = 0;
+for (i = 0; i <= 9; i++)
+{
+_putchar('i');
+}
+_putchar(' ');
+}
