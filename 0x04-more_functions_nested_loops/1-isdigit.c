816925b53daadeae5fda72f6e8d6551e8548b9b6
oxyvans <benja.1999.g@hotmail.com>
2022-02-09 04:36:51 -0800

    is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * @c : int
+ * Return: 1 sii c is digit
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
