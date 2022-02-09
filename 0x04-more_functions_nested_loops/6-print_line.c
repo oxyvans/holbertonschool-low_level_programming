9182dc4fe0eebd57addba1b64f557e7ba1755291
oxyvans <benja.1999.g@hotmail.com>
2022-02-09 05:43:37 -0800

    prin line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n : int
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+if (n > 0)
+{
+int j;
+for (j = 0; j <= n; j++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
