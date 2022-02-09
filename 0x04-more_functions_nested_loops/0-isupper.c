d89cc71c9a7bea912da63738fc770160f93e341c
oxyvans <benja.1999.g@hotmail.com>
2022-02-09 04:30:30 -0800

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ * @c : int
+ * Return: 1 sii c is upper
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
