26b5e8e3dabf1205ec7247bbbf17c863492ab878
oxyvans <benja.1999.g@hotmail.com>
2022-02-11 10:09:42 -0800

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - main
+ * @str : chars
+ * Return:
+ */
+
+void puts_half(char *str)
+{
+int i;
+ for ((i = ((_strlen(str)-1)/ 2)); i != '\0' ; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
