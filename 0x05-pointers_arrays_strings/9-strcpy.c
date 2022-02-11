299c727f70ff46ec92b02702666a899251ffbf25
oxyvans <benja.1999.g@hotmail.com>
2022-02-11 11:20:21 -0800

    9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,21 +5,21 @@
  * _strcpy - main
  * @dest : chars
  * @src : chars
- *
+ * Return : dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
 int i, n;
 char str;
-i = (_strlen(src) - 1);
-if (i >= 0)
-{  
+i = (_strlen(src));
+if (i > 0)
+{
 for (n = 0; n <= i; n++)
 {
 str = src[n];
 dest[n] = str;
 }
 }
-return (dest); 
+return (dest);
 }
