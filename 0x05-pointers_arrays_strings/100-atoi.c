6b7857c822a5e3b3c00065b423532397d232cf07
oxyvans <benja.1999.g@hotmail.com>
2022-02-11 14:38:20 -0800

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include "2-strlen.c"
-
+#include <stdbool.h>
 /**
  * _atoi - main
  * @s : chars
@@ -20,13 +20,13 @@ int _atoi(char *s)
 
   for (j = 0; j < i; j++)
     {
-      if(s[j] == 45)
+      if(s[j] == '-')
 	signo = (signo * -1);
 
 	while (s[j] >= 48 && s[j] <= 57)
 	{
 	 flag = true;
-	 res = (res * 10) = (s[j] - '0');
+	 res = (res * 10) + (s[j] - '0');
 	}
 
       if (flag)
