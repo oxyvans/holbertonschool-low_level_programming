cb61065885a25a8f211ab9bf28038709761fff88
oxyvans <benja.1999.g@hotmail.com>
2022-02-11 10:41:32 -0800

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-srtlen.c"
+#include <stdio.h>
+
+/**
+ * print_array - print
+ * @a : int
+ * @n : int
+ *
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+printf("%s", ", ");
+}
+peinrf("\n");
+}
