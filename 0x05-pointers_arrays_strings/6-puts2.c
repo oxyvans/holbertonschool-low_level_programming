a57a8f5c15a8f01bfcb648365218713e42e92c4b
oxyvans <benja.1999.g@hotmail.com>
2022-02-11 10:01:47 -0800

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - main
+ * @str : chars
+ *
+ */
+
+void puts2(char *str)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if ((i % 2) == 0)
+_putchar(s[i]);
+}
+_putchar('\n');
+}
