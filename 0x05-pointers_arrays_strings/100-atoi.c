b2f3e1823d526dd1c23c9164918ae9d654c0c983
oxyvans <benja.1999.g@hotmail.com>
2022-02-11 15:00:46 -0800

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,28 +11,27 @@
 
 int _atoi(char *s)
 {
-  unsigned int res = 0;
-  bool flag = false;
-  int i, j, signo;
-  i = _strlen(s);
-
-
-
-  for (j = 0; j < i; j++)
-    {
-      if(s[j] == '-'){
-	signo = (signo * -1);
-      }
-	while (s[j] >= '0' && s[j] <= '9')
-	{
-	 flag = true;
-	 res = (res * 10) + (s[j] - '0');
-	 j++;
-	}
-
-	if (flag){
-	break;
-	}
-    }
-  return (res * signo);
+unsigned int res = 0;
+bool flag = false;
+int i, j, signo;
+i = _strlen(s);
+signo = 1;
+for (j = 0; j < i; j++)
+{
+if (s[j] == '-')
+{
+signo = (signo * -1);
+}
+while (s[j] >= '0' && s[j] <= '9')
+{
+flag = true;
+res = (res * 10) + (s[j] - '0');
+j++;
+}
+if (flag)
+{
+break;
+}
+}
+return (res *signo);
 }
