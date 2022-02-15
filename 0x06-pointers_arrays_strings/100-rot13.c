a84f375d2998f097d403360661ecafe77c840c68
oxyvans <benja.1999.g@hotmail.com>
2022-02-15 11:40:25 -0800

    rot-13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * string_toupper - comp two strings
+ * @ : chars
+ *
+ *
+ * Return: string
+ */
+
+char *rot13(char *a)
+{
+int i, j;
+char a_z[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+for (i = 0; a[i] != '\0'; i++)
+{
+for (j = 0; a_z[j] != '\0'; j++)
+{
+if (a[i] == a_z[j])
+{
+a[i] = rot[j];
+break;
+}
+}
+}
+return (a);
+}
