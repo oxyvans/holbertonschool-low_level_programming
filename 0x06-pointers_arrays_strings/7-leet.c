52d2a1dfa9150e06ffc7dcac995e2c7ffeb2fd43
oxyvans <benja.1999.g@hotmail.com>
2022-02-15 10:11:05 -0800

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,24 +10,17 @@
 
 char *leet(char *a)
 {
-int i;
-for (i = 0; a[i] != '\n'; i++)
+int i, j;
+char l[] = "aAeEoOtTlL";
+char n[] = "4433007711";
+
+for (i = 0; a[i] != '\0'; i++)
 {
- if((a[i] == 'a') || (a[i] == 'A'))
-	a[i] = '4';
-
- if((a[i] == 'e') || (a[i] == 'E'))
-        a[i] = '3';
-
- if((a[i] == 'o') || (a[i] == 'O'))
-        a[i] = '0';
-
- if((a[i] == 't') || (a[i] == 'T'))
-        a[i] = '7';
-
- if((a[i] == 'l') || (a[i] == 'L'))
-        a[i] = '1';
-
+for (j = 0; l[j] != '\0'; j++)
+{
+if (a[i] == l[j])
+	a[i] = n[j];
+}
 }
 return (a);
 }
