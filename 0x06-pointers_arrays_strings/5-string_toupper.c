0f852561bba691307eb447fda262641a1e98018c
oxyvans <benja.1999.g@hotmail.com>
2022-02-15 08:58:41 -0800

    to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - comp two strings
+ * @ : chars
+ *
+ *
+ * Return: string
+ */
+
+char *string_toupper(char * a)
+{
+int i;
+char aux;
+for (i = 0; a[i] != '\0'; i++)
+{
+	if ((a[i] >= 'a') && (a[i] <= 'z'))
+		{
+		aux = a[i];
+		a[i] = (aux - 32);
+		}
+}
+return (a);
+}
