33e240c2e111019ae156b23ee1d4a3d3bbc1ae30
oxyvans <benja.1999.g@hotmail.com>
2022-02-15 07:29:21 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcmp - comp two strings
+ * @s2 : chars
+ * @s2 : chars
+ *
+ * Return: comp
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i, j;
+for (i = 0; s1[i] != '\0'; i++)
+{
+}
+for (j = 0; s2[j] != '\0'; j++)
+{
+}
+if (i == j)
+	return (0);
+else
+	if (i > j)
+		return (15);
+	else
+		return (-15);
+}
