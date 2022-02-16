628499ab23a31e42306ab27c11fc6488903b2d02
oxyvans <benja.1999.g@hotmail.com>
2022-02-16 03:57:38 -0800

    fix betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,24 +4,23 @@
  * _strncat -  concatenates two strings.
  * @dest : pointer
  * @src : pointer
- * @i : int
+ * @n : int
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  int i, j, k;
-  for (i = 0; dest[i] != '\0'; i++)
-    {
-    }
-  for (k = 0; src[k] != '\0'; k++)
-    {
-    }
-  
-  if (n > k)
-    n = k;
-  
-  for (j = 0; j < n; j++, i++)
-    {
-      dest[i] = src[j];
-    }
-  return (dest);
+int i, j, k;
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
+for (k = 0; src[k] != '\0'; k++)
+{
+}
+if (n > k)
+n = k;
+for (j = 0; j < n; j++, i++)
+{
+dest[i] = src[j];
+}
+return (dest);
 }
