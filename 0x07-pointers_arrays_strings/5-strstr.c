b23d6c7d085aacb6946eb35ae541c4bb896f83bb
oxyvans <benja.1999.g@hotmail.com>
2022-02-21 11:36:17 -0800

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,42 +1,38 @@
 #include "main.h"
-
 /**
- * _strstr - man
- * compare - static
- * @needle : pointer
- * @haystack : pointer
- * @X : x
- * @Y : y
- * Return: char
+ * compare - man
+ * @X : pointer
+ * @Y : pointer
+ * Return: int
  */
-
-
 int compare(const char *X, const char *Y)
 {
-    while (*X && *Y)
-    {
-        if (*X != *Y) {
-            return 0;
-        }
- 
-        X++;
-        Y++;
-    }
- 
-    return (*Y == '\0');
+while (*X && *Y)
+{
+if (*X != *Y)
+{
+return (0);
 }
-
+X++;
+Y++;
+}
+return (*Y == '\0');
+}
+/**
+ * _strstr - man
+ * @needle : pointer
+ * @haystack : pointer
+ * Return: char
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	if (needle[0] == '\0')
-		return (haystack);
-	 while (*haystack != '\0')
-    {
-        if ((*haystack == *needle) && compare(haystack, needle)) {
-            return haystack;
-        }
-        haystack++;
-    }
- 
-    return NULL;
+if (needle[0] == '\0')
+	return (haystack);
+while (*haystack != '\0')
+{
+if (((*haystack == *needle) && compare(haystack, needle)))
+return (haystack);
+haystack++;
+}
+return (NULL);
 }
