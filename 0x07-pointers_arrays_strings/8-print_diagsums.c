f079b8a2ecb9c1a446612de645bcd5fb88af7d82
oxyvans <benja.1999.g@hotmail.com>
2022-02-21 12:53:44 -0800

    8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,17 +7,21 @@
  */
 void print_diagsums(int *a, int size)
 {
-int i, suma = 0, suma2 = 0;
-for (i = 0; i < size; i++)
-{
-	suma = suma + a[i];
-	a = a + size;
-}
-a = a - size;
-for (i = 0; i < size; i++)
-{
-	suma2 = suma2 + a[i];
-	a = a - size;
-}
-printf("%d ,%d\n", suma, suma2);
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
 }
