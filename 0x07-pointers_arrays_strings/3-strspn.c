583677d5f383bd43b201f0fb711dad3b890397bf
oxyvans <benja.1999.g@hotmail.com>
2022-02-21 08:17:29 -0800

    fix betty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _strspn - number of bytes in the initial segment of s which consist only of bytes from accept
+ * _strspn - main
  * @s : pointer
- * @saccept : pointer
+ * @accept : pointer
  * Return: int
  */
 unsigned int _strspn(char *s, char *accept)
 {
 int i, j, res = 0, flag = 0;
-for ( i = 0; s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 	for (j = 0; accept[j] != '\0'; j++)
 	{
