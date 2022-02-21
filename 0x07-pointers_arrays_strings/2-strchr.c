88caf306b9cb0fe3957590990deff3705ed260b7
oxyvans <benja.1999.g@hotmail.com>
2022-02-21 08:08:13 -0800

    fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * _strchr - pointer to the first occurrence of the character c in the string s, or NULL if the character is not found 
+ * _strchr - n 
  * @s : pointer
  * @c : char
  * Return: char
  */
 char *_strchr(char *s, char c)
 {
+if (s != NULL)
 while (*s)
 {
 	if (*s == c)
