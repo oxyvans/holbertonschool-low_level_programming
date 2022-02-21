38a407027aa31a08a7ff481dc20e6d8c92bd6a21
oxyvans <benja.1999.g@hotmail.com>
2022-02-21 09:05:45 -0800

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _strpbrk - function locates the first occurrence in the string s of any of the bytes in the string accept
+ * _strpbrk - main
  * @s : pointer
- * @saccept : pointer
+ * @accept : pointer
  * Return: char
  */
 char *_strpbrk(char *s, char *accept)
 {
 int i = 0, j, flag = 0;
-while(*s)
+while (*s)
 {
 	for (j = 0; accept[j] != '\0'; j++)
 	if (s[i] == accept[j])
