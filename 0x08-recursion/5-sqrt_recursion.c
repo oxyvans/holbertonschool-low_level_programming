52b89663fa561a61256148cce281f032cd8375ad
oxyvans <benja.1999.g@hotmail.com>
2022-02-23 07:46:26 -0800

    bestiasss

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - main
+ * aux - main
  * @n : int
+ * @i : int
+ * @original : origin
  * Return: int
  */
 
@@ -10,22 +12,24 @@
 int aux(int n, int i, int original)
 {
 if (n == 1)
-		return (-1);
+	return (-1);
 if (n % 2 == 0)
 	n = original - 1;
-
 i = n / 2;
-
 if ((i * i) == original)
 	return (i);
-return aux (n - 2, i, original);
+return (aux(n - 2, i, original));
 }
 
-
+/**
+ * _sqrt_recursion - main
+ * @n : int
+ * Return: int
+ */
 
 int _sqrt_recursion(int n)
 {
 if (n < 0)
 	return (-1);
-return aux(n, n, n);
+return (aux(n, n, n));
 }
