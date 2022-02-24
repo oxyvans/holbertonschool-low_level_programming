cb8c358a837629150a11b2eaab72de4ba4b1ec35
oxyvans <benja.1999.g@hotmail.com>
2022-02-23 17:17:36 -0800

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,8 +3,8 @@
 
 /**
  * aux - main
- * @
- * @
+ * @s1 : char
+ * @s2 : char
  * Return: int
  */
 
@@ -12,28 +12,37 @@ int aux(char *s1, char *s2)
 {
 if (*s1 == 0 && *s2 == 0)
 	return (1);
+
 if (*s1 == 0 && *s2 == '*')
 	return (aux(s1, s2 + 1));
+
 if (*s1 == 0 || *s2 == 0)
 	return (0);
+
 if (*s1 == *s2)
 	return (aux(s1 + 1, s2 + 1));
 
+if (*s2 == '*')
+{
+	if (*(s2 + 1) == '*')
+		return (aux(s1, s2 + 1));
+	if (aux(s1, s2 + 1) == 1)
+		return (1);
+	return (aux(s1 + 1, s2));
+}
+return (0);
 }
 
 /**
  * wildcmp - main
- * @n : int
+ * @s1 : char
+ * @s2 : char
  * Return: int
  */
 
 int wildcmp(char *s1, char *s2)
 {
-int size_s1 = _strlen_recursion(s1);
-int size_s2 = _strlen_recursion(s2);
-if (size_s1 != size_s2)
-	return (0);
-return (aux(s1, s2);
+return (aux(s1, s2));
 }
 
 
