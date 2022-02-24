3e8aa8a75f6e784d516857fd35931fef19d7d716
oxyvans <benja.1999.g@hotmail.com>
2022-02-23 17:22:57 -0800

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,38 +2,6 @@
 #include "2-strlen_recursion.c"
 
 /**
- * aux - main
- * @s1 : char
- * @s2 : char
- * Return: int
- */
-
-int aux(char *s1, char *s2)
-{
-if (*s1 == 0 && *s2 == 0)
-	return (1);
-
-if (*s1 == 0 && *s2 == '*')
-	return (aux(s1, s2 + 1));
-
-if (*s1 == 0 || *s2 == 0)
-	return (0);
-
-if (*s1 == *s2)
-	return (aux(s1 + 1, s2 + 1));
-
-if (*s2 == '*')
-{
-	if (*(s2 + 1) == '*')
-		return (aux(s1, s2 + 1));
-	if (aux(s1, s2 + 1) == 1)
-		return (1);
-	return (aux(s1 + 1, s2));
-}
-return (0);
-}
-
-/**
  * wildcmp - main
  * @s1 : char
  * @s2 : char
@@ -42,7 +10,27 @@ return (0);
 
 int wildcmp(char *s1, char *s2)
 {
-return (aux(s1, s2));
+if (*s1 == 0 && *s2 == 0)
+	return (1);
+
+if (*s1 == 0 && *s2 == '*')
+	return (wildcmp(s1, s2 + 1));
+
+if (*s1 == 0 || *s2 == 0)
+	return (0);
+
+if (*s1 == *s2)
+	return (wildcmp(s1 + 1, s2 + 1));
+
+if (*s2 == '*')
+{
+	if (*(s2 + 1) == '*')
+		return (wildcmp(s1, s2 + 1));
+	if (wildcmp(s1, s2 + 1) == 1)
+		return (1);
+	return (wildcmp(s1 + 1, s2));
+}
+return (0);
 }
 
 
