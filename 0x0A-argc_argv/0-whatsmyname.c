aa639452da26bbe3489f2b6aab1f578e8fbf7a47
oxyvans <benja.1999.g@hotmail.com>
2022-02-25 03:49:31 -0800

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main
+ * @argc : size
+ * @argv : char
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc >= 0)
+{
+	printf("%s\n", argv[0]);
+}
+return (0);
+}
