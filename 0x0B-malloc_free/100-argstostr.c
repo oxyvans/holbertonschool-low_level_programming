88168ac2e92212bb5e8721ca2c70ce303e3fd88e
oxyvans <benja.1999.g@hotmail.com>
2022-03-02 11:23:06 -0800

    100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,27 +12,33 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i = 1, len = 0, j;
+	int len = 0, j, i, c = 0;
 	char *res;
 
-	if (ac == 0 || *av == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (j = 0; j < ac; j++)
-		len += (strlen(av[j]) + 1);
+		len += ((int)strlen(av[j]) + 1);
 
 	res = malloc((len + 1) * sizeof(char));
 
 	if (res == NULL)
 		return (NULL);
 
-	res[0] = *av[0];
-	while (i <= len)
+	for (i = 0; i < ac; i++)
 	{
-		if (i % 2 == 0)
-			res[i] = '\n';
-		res[i] = *av[i];
-		i++;
+		j = 0;
+		while (av[i][j])
+		{
+			res[c] = av[i][j];
+			j++;
+			c++;
+		}
+		res[c] = '\n';
+		c++;
 	}
+	res[c] = '\0';
+
 	return (res);
 }
