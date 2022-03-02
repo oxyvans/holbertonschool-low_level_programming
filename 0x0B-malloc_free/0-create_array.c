fcd5f99575c4f8cbbe0a1f588edbb8a8a859aabe
oxyvans <benja.1999.g@hotmail.com>
2022-03-02 05:06:11 -0800

    00

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (0);
 
-	res = malloc(size * sizeof(char));
+	res = (char*)malloc(size * sizeof(char));
 
 	for (i = 0; i <= size; i++)
 	{
