06c83fdbc2fdb65dd7174951b844a4ad080c7470
oxyvans <benja.1999.g@hotmail.com>
2022-03-02 10:53:25 -0800

    100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * argstostr - main
@@ -11,19 +12,22 @@
 
 char *argstostr(int ac, char **av)
 {
-	int i = 1;
+	int i = 1, len = 0, j;
 	char *res;
 
 	if (ac == 0 || *av == NULL)
 		return (NULL);
 
-	res = malloc(((ac * 2) - 1) * sizeof(char));
+	for (j = 0; j < ac; j++)
+		len += (strlen(av[j]) + 1);
+
+	res = malloc((len + 1) * sizeof(char));
 
 	if (res == NULL)
 		return (NULL);
 
 	res[0] = *av[0];
-	while (i <= ((ac * 2) - 1))
+	while (i <= len)
 	{
 		if (i % 2 == 0)
 			res[i] = '\n';
