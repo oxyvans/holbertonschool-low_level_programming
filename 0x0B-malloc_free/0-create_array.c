81c5db6f3096b73e31458bd4aec1d5d56cc8b73a
oxyvans <benja.1999.g@hotmail.com>
2022-03-02 04:52:10 -0800

    00

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_array - main
+ * @size : size
+ * @c : char
+ * Return: chars
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *res;
+	unsigned int i;
+
+	if (size == 0)
+		return (0);
+
+	res = malloc(size * sizeof(char));
+
+	for (i = 0; i <= size; i++)
+	{
+		res[i] = c;
+	}
+
+
+	return (res);
+}
