60b4f2f4a553f19a2099202567ef0df5798e8b1d
oxyvans <benja.1999.g@hotmail.com>
2022-03-02 06:56:56 -0800

    02

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,17 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+	if (s1 != NULL)
+	{
 	for (size1 = 0; s1[size1] != '\0'; size1++)
 	{
 	}
+	}
+	if (s2 != )
+	{
 	for (size2 = 0; s2[size2] != '\0'; size2++)
 	{
+	}
 	}
 	res = malloc((size1 + size2 + 1) * sizeof(char));
 	if (res == NULL)
