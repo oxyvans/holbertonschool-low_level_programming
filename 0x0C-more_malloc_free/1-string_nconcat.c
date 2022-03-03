f0780ee484563e0cbcc4ec990eb25d530b746734
oxyvans <benja.1999.g@hotmail.com>
2022-03-03 08:15:09 -0800

    01

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *res;
-	unsigned int i, j, size1 = 0, size2 = 0;
+	unsigned int i = 0, j, size1 = 0, size2 = 0;
 
 	if (s1 != NULL)
 	{
