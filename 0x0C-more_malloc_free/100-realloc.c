7e4613bc9a568b81c79ea94ad5082332ed885000
oxyvans <benja.1999.g@hotmail.com>
2022-03-03 09:56:31 -0800

    100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _realloc - main
+ * @ptr : int
+ * @old_size : int
+ * @new_size : int
+ * Return: array
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *ptrNew;
+
+	if (old_size >= new_size)
+		return (ptr);
+
+	if ((ptr != NULL) && (new_size == 0))
+	{
+		free(ptr);
+		return (NULL);
+	}
+	
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	
+	ptrNew = malloc(new_size);
+
+	if (ptrNew == NULL)
+		return (NULL);
+
+	memcpy(ptrNew, ptr, old_size);
+	free(ptr);
+	return (ptrNew);
+}
