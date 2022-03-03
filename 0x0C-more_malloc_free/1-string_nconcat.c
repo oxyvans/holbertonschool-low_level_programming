dbd2abed8d9316ae294b74d6953456d6e842e5fc
oxyvans <benja.1999.g@hotmail.com>
2022-03-03 08:08:29 -0800

    01

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,33 +17,38 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 != NULL)
 	{
-	for (size1 = 0; s1[size1] != '\0'; size1++)
-	{
+		for (size1 = 0; s1[size1] != '\0'; size1++)
+		{
+		}
 	}
-	}
+
 	if (s2 != NULL)
 	{
-	for (size2 = 0; s2[size2] != '\0'; size2++)
-	{	
+		for (size2 = 0; s2[size2] != '\0'; size2++)
+		{	
+		}
 	}
-	}
+
 	if (n < size2 + 1)
 		size2 = n;
 
 	res = malloc((size1 + size2 + 1) * sizeof(char));
+
 	if (res == NULL)
 		return (NULL);
 
 	if (s1 != NULL)
-		for (i = 0; i < size1; i++)
+		for (i = 0; s[i] != '\0'; i++)
 		{
 			res[i] = s1[i];
 		}
+
 	if (s2 != NULL)
 		for (j = 0; j < size2; i++, j++)
 		{
 			res[i] = s2[j];
 		}
+
 	res[0] = '\0'; 
 	return (res);
 }
