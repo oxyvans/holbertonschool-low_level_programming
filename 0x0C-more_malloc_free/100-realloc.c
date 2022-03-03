2a6b2371a5a0aebc0804ede4691f46293f399a10
oxyvans <benja.1999.g@hotmail.com>
2022-03-03 10:05:39 -0800

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,22 +14,21 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *ptrNew;
-
-	if (old_size >= new_size)
+	if (ptr == NULL)
+	 {
+		ptr = malloc(new_size);
 		return (ptr);
+	 }
 
 	if ((ptr != NULL) && (new_size == 0))
 	{
 		free(ptr);
 		return (NULL);
 	}
-	
-	if (ptr == NULL)
-	{
-		ptr = malloc(new_size);
+
+	if (old_size >= new_size)
 		return (ptr);
-	}
-	
+
 	ptrNew = malloc(new_size);
 
 	if (ptrNew == NULL)
