71b56c4d843bf7e166d3c1eeca3714effb0724e8
oxyvans <benja.1999.g@hotmail.com>
2022-03-08 06:17:05 -0800

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - main
+ * @d : dog
+ * @name : name dog
+ * @age : age dog
+ * @owner : owuner dog
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+}
