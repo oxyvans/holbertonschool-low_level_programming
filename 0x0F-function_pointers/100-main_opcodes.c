5abda8adaff63dc2dca21a79fda8d4b6f39ecaa0
oxyvans <benja.1999.g@hotmail.com>
2022-03-09 15:41:32 -0800

    100

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the opcodes of his function
+ * @argc: int
+ * @argv: chars
+ * Return: O
+ */
+
+int main(int argc, char argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	return (0);
+}
