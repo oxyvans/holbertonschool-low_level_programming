ac8bb287bfae25f67fa90cd2a2c2c9035bc0d58e
oxyvans <benja.1999.g@hotmail.com>
2022-03-09 07:53:42 -0800

    add condicion

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0 || cmp == NULL || array == NULL)
+	if (size < 0 || cmp == NULL || array == NULL)
 	{
 		return (-1);
 	}
