21bda9c2dfe0b50daa24a65e5aa41c6ea1edd962
oxyvans <benja.1999.g@hotmail.com>
2022-03-09 07:35:59 -0800

    start 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,14 @@
+#include "3-calc.h"
+
+/**
+ * op_add - main
+ * @get_op_func : char
+ * @s : int
+ * @int : int
+ * Return: int
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,12 @@
+#include "3-calc.h"
+
+/**
+ * main - main
+ * @argc : int
+ * @argv : int
+ */
+
+void main(int argc, char *argv[])
+{
+
+}
