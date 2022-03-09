e21aa7a08c3be2b6f4d195e25affacf0f6e9f145
oxyvans <benja.1999.g@hotmail.com>
2022-03-09 06:30:41 -0800

    3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - main
+ * @a : int
+ * @b : int
+ * Return: op
+ */
+
+int op_add(int a, int b)
+{
+
+}
+
+/**
+ * op_sub - main
+ * @a : int
+ * @b : int
+ * Return: op
+ */
+
+int op_sub(int a, int b)
+{
+
+}
+
+/**
+ * op_mul - main
+ * @a : int
+ * @b : int
+ * Return: op
+ */
+
+int op_mul(int a, int b)
+{
+
+}
+
+/**
+ * op_div - main
+ * @a : int
+ * @b : int
+ * Return: op
+ */
+
+int op_div(int a, int b)
+{
+
+}
+
+/**
+ * op_mod - main
+ * @a : int
+ * @b : int
+ * Return: op
+ */
+
+int op_mod(int a, int b)
+{
+
+}
