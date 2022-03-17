d01f31956cee2e582237069d9bf317da99b092a0
oxyvans <benja.1999.g@hotmail.com>
2022-03-17 09:52:25 -0700

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * print_before_main - print
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
