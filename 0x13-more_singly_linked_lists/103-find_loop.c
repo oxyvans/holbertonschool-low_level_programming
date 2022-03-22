af2cc5178a5b2e547ba8289623aa42a8ddb95b4c
oxyvans <benja.1999.g@hotmail.com>
2022-03-22 12:07:43 -0700

    103

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - main
+ * @head: list.
+ *
+ * Return: list
+ **/
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	if (head == NULL)
+		return (NULL);
+
+	return (NULL);
+}
