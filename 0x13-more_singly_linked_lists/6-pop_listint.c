1021a483232659b489e61aae7b3b3dbcdbc77a4b
oxyvans <benja.1999.g@hotmail.com>
2022-03-22 05:50:38 -0700

    06

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,9 +12,13 @@ int pop_listint(listint_t **head)
 	int res;
 	listint_t *aux;
 
+	if (!(*head))
+		return (0);
+
 	res = (*head)->n;
 	aux = (*head)->next;
 	free(*head);
 	*head = aux;
+
 	return (res);
 }
