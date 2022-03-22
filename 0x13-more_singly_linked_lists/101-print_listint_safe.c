71c62b6dfeab401de053939d1c69df7895f29fd2
oxyvans <benja.1999.g@hotmail.com>
2022-03-22 11:56:25 -0700

    101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - main
+ * @head: list.
+ *
+ * Return: size_t.
+ **/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t cont = 0;
+
+	if (head == NULL)
+		exit(98);
+
+	return (cont);
+}
