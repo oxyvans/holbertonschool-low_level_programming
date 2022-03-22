8def3703e4631f592a26727ba39d5b39217e86b6
oxyvans <benja.1999.g@hotmail.com>
2022-03-22 05:44:33 -0700

    fix betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,21 +13,21 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *aux = *head;
 	listint_t *new;
 	unsigned int i = 0;
-	
+
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
-	
+
 	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
 		return (*head);
 	}
-	
+
 	while (aux && (i != (idx - 1)))
 	{
 		aux = aux->next;
@@ -35,11 +35,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	if ((i == (idx - 1)) && aux)
-	{	
+	{
 		new->next = aux->next;
 		aux->next = new;
 		return (new);
 	}
-	
+
 	return (NULL);
 }
