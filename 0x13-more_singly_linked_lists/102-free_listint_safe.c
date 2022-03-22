12812c160a9e88aeb4c72abe65e07fbd3de8a7cf
oxyvans <benja.1999.g@hotmail.com>
2022-03-22 12:00:32 -0700

    102

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - main
+ * @h: list.
+ *
+ * Return: size_t.
+ **/
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t cont = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	return (cont);
+}
