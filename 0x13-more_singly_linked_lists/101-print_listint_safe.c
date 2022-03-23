f1edfcdc85f85e02c6d0a09b2ef4635ef4ca2041
oxyvans <benja.1999.g@hotmail.com>
2022-03-23 10:11:04 -0700

    101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,10 +9,29 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *slow_p = head, *fast_p = head;
 	size_t cont = 0;
 
 	if (head == NULL)
-		exit(98);
+		return (cont);
+
+	fast_p = head->next;
+	slow_p = head;
+
+	while (fast_p && fast_p < slow_p)
+	{
+		printf("[%p] %i\n", (void *)slow_p, slow_p->n);
+		slow_p = slow_p->next;
+		fast_p = fast_p->next;
+		cont++;
+	}
+	printf("[%p] %i\n", (void *)slow_p, slow_p->n);
+	cont++;
+
+	if (fast_p)
+	{
+		printf("-> [%p] %i\n", (void *)fast_p, fast_p->n);
+	}
 
 	return (cont);
 }
