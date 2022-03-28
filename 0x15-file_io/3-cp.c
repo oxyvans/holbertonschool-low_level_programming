276940950b49674f3abf06296913efed01995fb6
oxyvans <benja.1999.g@hotmail.com>
2022-03-28 16:58:00 -0700

    03

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,35 @@
 #include "main.h"
 
 /**
+ * aux - main
+ * @argv1 : char
+ * @argv2 : char
+ * @r: int
+ * @fd_f: int
+ * @fd_t: int
+ */
+
+void error_(char *argv1, char *argv2, ssize_t r, int fd_f, int fd_t)
+{
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv1);
+		exit(98);
+	}
+
+	if (close(fd_f) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *argv1);
+		exit(100);
+	}
+	if (close(fd_t) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *argv2);
+		exit(100);
+	}
+}
+
+/**
  * main - main
  * @argc : size
  * @argv : char
@@ -18,27 +47,18 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	/*------------------ create -------------- */
-
 	fd_f = open(argv[1], O_RDONLY);
-
 	if (fd_f == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	if (fd_t == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
-	/*------------------ read -------------- */
-
 	while ((r = read(fd_f, buf, 1024)) > 0)
 	{
 		if (r == -1)
@@ -46,34 +66,13 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-
-		/*------------------ write -------------- */
-
 		w = write(fd_t, buf, r);
-
 		if (w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-
 	}
-
-	if (r == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	if (close(fd_f) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *argv[1]);
-		exit(100);
-	}
-	if (close(fd_t) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", *argv[2]);
-		exit(100);
-	}
+	error_(argv[1], argv[2], r, fd_f, fd_t);
 	return (0);
 }
