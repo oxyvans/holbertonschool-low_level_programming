142ffbf06131bf3a09bd7cde8ba01714b66fe561
oxyvans <benja.1999.g@hotmail.com>
2022-03-28 11:32:43 -0700

    02

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - main
+ * @filename: char
+ * @text_content: size
+ * Return: int
+ **/
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t o, fd, i = 0;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	while (text_content[i] != '\0')
+		i++;
+
+	o = write(fd, text_content, i);
+
+	if (o != i)
+		return (0);
+
+	close(fd);
+	return (1);
+
+}
