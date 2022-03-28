beb5a6cfd6196e3e459fb362ab1dfe0d87720a56
oxyvans <benja.1999.g@hotmail.com>
2022-03-28 11:49:22 -0700

    02

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -33,8 +33,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	o = write(fd, text_content, i);
 
 	if (o != i)
+	{
 		close(fd);
 		return (0);
+	}
 
 	close(fd);
 	return (1);
