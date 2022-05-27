6100924214044911b0e8562f61651ff9991d9d8d
oxyvans <benja.1999.g@hotmail.com>
2022-05-27 07:39:49 -0700

    add task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - index of a key
+ * @key: key
+ * @size: size
+ * Return: unsigned long int
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) %  size);
+}
