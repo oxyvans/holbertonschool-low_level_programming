bac0eb46c970d61b95f6929d64d9d6774260c407
oxyvans <benja.1999.g@hotmail.com>
2022-08-26 16:17:45 -0700

    add task 0

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,9 +1,9 @@
-#ifndef SEARCH_H
-#define SEARCH_H
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 #include <stdio.h>
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
-#endif
+#endif /* SEARCH_ALGOS_H */
